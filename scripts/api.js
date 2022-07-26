bd3e3ba475407515cfbf4125c5d81ede802dae73
Bruno Calado <brunocalado@users.noreply.github.com>
2022-07-26 14:29:47 -0300

    0.0.4

--- scripts/api.js
+++ scripts/api.js
@@ -1,17 +1,16 @@
 export class cwb {
-
+  // -------------------------------------------------------------
+  //  
   static async getCompendiumEntities( entityType, compendiumLabel ) {
     if ( !this.validEntityType(entityType) ) return;    
-    
-    const compendiums = game.packs.filter(p=>p.documentName==entityType); // list all compendium from the type     
+    const index = await game.packs.find(p=>p.metadata.label=='spirit charms').getIndex({fields: ["type"]});
     if (!compendiums) this.debug( "The compendium couldn't be found." );
-    const inside = await compendiums.filter( p=>p.metadata.label==compendiumLabel )[0].getDocuments();     
-    if (!inside) this.debug( "The compendium is empty." );
-    return inside;
+    //console.log(index.map(i=>i.type));
+    return index;
   }    
   
   static validEntityType( entityType ) {
-    const entityTypes = ["Actor", "JornalEntry", "Scene", "Item", "Cards"]; // valid entity?    
+    const entityTypes = ["Actor", "Item"]; // valid entity?    
     const result = entityTypes.includes(entityType);
     if ( !result ) this.debug( entityType + ' is not a valid type!' );
     return result;
@@ -22,6 +21,7 @@ export class cwb {
   
     console.warn('========================');
     for (let compendium of compendiums) {      
+      console.log('ID: ' + compendium.id);
       console.log('Label: ' + compendium.title);
       console.log('Name: ' + compendium.metadata.name);
       console.log('Package: ' + compendium.metadata.package);
@@ -37,12 +37,6 @@ export class cwb {
       case 'Actor':
         entity = await Actor.createDocuments([data]);
         return;
-      case 'JournalEntry':
-        entity = await JournalEntry.createDocuments([data]);
-        return;
-      case 'Scene':
-        entity = await Scene.createDocuments([data]);
-        return;
       case 'Item':       
         entity = await Item.createDocuments([data]);      
         return;        
@@ -50,6 +44,96 @@ export class cwb {
         this.debug( entityType + ' is not a valid type!' );
     }    
   }
+
+  // -------------------------------------------------------------
+  // UI
+  static showEntityData() {  
+
+
+    let template = `  
+      <h2>head</h2>
+      
+      <div class="divTable purpleHorizon">
+      <div class="divTableBody">
+      
+      <div class="divTableRow" align="center">
+      <div class="divTableCell">
+          <b>Entity Type</b> 
+          <select id="entityType" type="text">
+            <option value="item">Item</option>
+            <option value="actor">Actor</option>
+          </select>      
+      </div>    
+      </div>
+      
+      </div>
+      </div>    
+      
+      <br>
+
+      <div class="divTable purpleHorizon">
+      <div class="divTableBody">
+      
+      <div class="divTableRow" align="center">
+      <div class="divTableCell">
+          <b>Item Name existing in the World</b> 
+          <input id="itemName" type="text" value="">        
+      </div>    
+      </div>
+      
+      </div>
+      </div>  
+      
+      <br>
+
+      <div class="divTable purpleHorizon">
+      <div class="divTableBody">
+      
+      <div class="divTableRow" align="center">
+      <div class="divTableCell">
+          <b>Item Name existing in the Compendium</b> 
+          <input id="itemNameCompendium" type="text" value="">             
+      </div>    
+      </div>
+      
+      </div>
+      </div>  
+      
+      <br>     
+      
+      <div class="divTable purpleHorizon">
+      <div class="divTableBody">
+      
+      <div class="divTableRow" align="center">
+      <div class="divTableCell">
+          <b>Compedium Label</b> 
+          <input id="itemLabel" type="text" value="">             
+      </div>    
+      </div>
+      
+      </div>
+      </div>  
+      
+      <br>      
+    `;
+    
+    new Dialog({
+      title: `Entity Compare`,
+      content: template,
+      buttons: {
+        ok: {
+          label: "Show!",
+          callback: async (html) => {
+            //const skillSupporter = html.find("#skillAttacker")[0].value;    
+          },
+        },
+        cancel: {
+          label: "Cancel",
+        }
+      },
+      default: "ok"
+    }, {}).render(true);
+  }  
   
   // -------------------------------------------------------------
   // DEBUG
